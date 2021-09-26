19c0af5ccc2edf336b19b26eb8211b468adca024
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2021-09-26 21:21:45 +0200

    Update settings.py

--- databaseproject/databaseproject/settings.py
+++ databaseproject/databaseproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'databaseproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PizzaDatabase',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
