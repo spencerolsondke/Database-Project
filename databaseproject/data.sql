9c411d97fc66905471c23e14e97c31ccecc70b6d
Aurélien <a.aurelien@student.maastrichtuniversity.nl>
2021-10-01 17:37:11 +0200

    Working model

---
+++ databaseproject/data.sql
@@ -0,0 +1,26 @@
+USE pizzadb;
+
+/*
+INSERT INTO pizza (name) VALUES ('Prosciutto e funghi');
+INSERT INTO pizza (name) VALUES ('Detroit-Style');
+INSERT INTO pizza (name) VALUES ('Capricciosa');
+INSERT INTO pizza (name) VALUES ('Quattro fromaggi'); # Vegetarian
+INSERT INTO pizza (name) VALUES ('Caprese'); # Vegetarian
+INSERT INTO pizza (name) VALUES ('Calzone');
+INSERT INTO pizza (name) VALUES ('Margherita'); # Vegetarian
+INSERT INTO pizza (name) VALUES ('Napoletana'); # Vegetarian
+INSERT INTO pizza (name) VALUES ('Diavola');
+INSERT INTO pizza (name) VALUES ('Melanzane'); # Vegetarian
+INSERT INTO pizza (name) VALUES ('Hawaii');
+INSERT INTO pizza (name) VALUES ('Frutti di Mare');
+INSERT INTO pizza (name) VALUES ('Tonno');
+INSERT INTO pizza (name) VALUES ('Crudo');
+INSERT INTO pizza (name) VALUES ('Al Pesto');
+*/
+
+INSERT INTO drink (name) VALUES ('Fanta');
+INSERT INTO drink (name) VALUES ('Cola');
+INSERT INTO drink (name) VALUES ('Mineral water');
+INSERT INTO drink (name) VALUES ('Jupiler');
+INSERT INTO drink (name) VALUES ('Sparkling water');
+
--- databaseproject/index/models.py
+++ databaseproject/index/models.py
@@ -3,121 +3,99 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `managed = True` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Area(models.Model):
-    id = models.BigAutoField(primary_key=True)
     code = models.CharField(max_length=45)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'area'
 
 
+class Product(models.Model):
+    price = models.FloatField()
+
+    class Meta:
+        managed = True
+        db_table = 'product'
+
+
 class Customer(models.Model):
-    id = models.BigAutoField(primary_key=True)
     area = models.ForeignKey(Area, models.DO_NOTHING, blank=True, null=True)
     address = models.CharField(max_length=45)
     name = models.CharField(max_length=45)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'customer'
 
 
-class Dessert(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=45)
-
-    class Meta:
-        managed = False
-        db_table = 'dessert'
-
-
-class Drink(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=45)
-
-    class Meta:
-        managed = False
-        db_table = 'drink'
-
-
-class OrderToProduct(models.Model):
-    order = models.ForeignKey('Orders', models.DO_NOTHING)
-    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'order_to_product'
-
-
 class Orders(models.Model):
-    id = models.BigAutoField(primary_key=True)
     customer = models.ForeignKey(Customer, models.DO_NOTHING, blank=True, null=True)
-    restaurant = models.ForeignKey('Restaurant', models.DO_NOTHING, blank=True, null=True)
     status = models.CharField(max_length=45)
     order_time = models.DateTimeField()
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'orders'
 
 
-class Pizza(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=45)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        managed = False
-        db_table = 'pizza'
-
-
-class Product(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    price = models.FloatField()
-
-    class Meta:
-        managed = False
-        db_table = 'product'
-
-
-class Restaurant(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=45)
-
-    class Meta:
-        managed = False
-        db_table = 'restaurant'
-
-
 class DeliveryPerson(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    restaurant = models.ForeignKey('Restaurant', models.DO_NOTHING, blank=True, null=True)
     name = models.CharField(max_length=45)
     area = models.ForeignKey(Area, models.DO_NOTHING, blank=True, null=True)
-    availibility = models.BooleanField()
+    availibility = models.IntegerField()
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'delivery_person'
 
 
-class Toppings(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    vegetarian = models.BooleanField()
+class Dessert(models.Model):
+    product = models.ForeignKey(Product, models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=45)
 
     class Meta:
-        managed = False
+        managed = True
+        db_table = 'dessert'
+
+
+class Drink(models.Model):
+    product = models.ForeignKey(Product, models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        managed = True
+        db_table = 'drink'
+
+
+class OrderToProduct(models.Model):
+    order = models.ForeignKey(Orders, models.DO_NOTHING)
+    product = models.ForeignKey(Product, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'order_to_product'
+
+
+class Pizza(models.Model):
+    product = models.ForeignKey(Product, models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        managed = True
+        db_table = 'pizza'
+
+
+class Toppings(models.Model):
+    name = models.CharField(max_length=45)
+    vegetarian = models.IntegerField()
+
+    class Meta:
+        managed = True
         db_table = 'toppings'
 
 
@@ -126,5 +104,5 @@ class ToppingsToPizza(models.Model):
     toppings = models.ForeignKey(Toppings, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'toppings_to_pizza'
