5c779b8c0345b5390bd2845ce71bcb6683d30181
spencermo <mateiolson@gmail.com>
2021-10-05 14:15:10 +0200

    Admin page

--- databaseproject/index/urls.py
+++ databaseproject/index/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
 ]
